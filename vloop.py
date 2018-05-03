69389300428a7b5e61928dd7f17d83c481f3e108
klkanthudu <kanthudu02@gmail.com>
2018-05-03 10:26:56 +0530

    this is loop for python

---
+++ vloop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+print "enter the 1st number:$n\n";
+if ($n <= 50) {
+	print "number is above the 50\n";
+} else {
+	print "number is below the 50\n";
+} ## end if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
