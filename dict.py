b31cb3611bc58d33966967c3332314c836813058
klkanthudu <kanthudu02@gmail.com>
2018-05-03 10:03:41 +0530

    this is my file under python

---
+++ dict.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+my_dict={}.fromkeys(['englsh','hindhi','telugu','maths'],88);
+print(my_dict);
+print(my_dict.keys());
+print(my_dict.values());
+for k,v in my_dict.items():
+	print(k,v);
+
+
+
+
+
+my_list=["name",584,64654,7854];
+print(my_list);
+
+
+
+
+n=[4,5,6,456,789];
+for b in n:
+	print(b);
+
+
+
+
+
+
+
+
+
+
+
